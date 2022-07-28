2f299a9c5c843a7da029d2081e8fea9d604b1a08
Sarah Allen <sarah@ultrasaurus.com>
2022-07-27 22:40:17 -0700

    wip: iterate over files, subdirectories not mirrored, warnings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::{AppSettings, Parser};
 use pulldown_cmark::{html, Options, Parser as MarkdownParser};
 use std::fs;
-use std::io::{BufWriter, Read};
+use std::io::{BufWriter, Read, Write};
 use std::path::Path;
 use walkdir::{DirEntry, WalkDir};
 
@@ -49,16 +49,20 @@ fn clean_folder(path: &str) -> std::io::Result<()> {
 //     // copy media files
 // }
 
-fn is_markdown_filename(s: &str) -> bool {
-    s.ends_with(".md") || s.ends_with(".markdown")
+// if path references a markdown file, return filename stem
+// index.md -> Some(index)
+// thing.png -> None
+fn markdown_filename(path: &Path) -> Option<&str> {
+    if let Some(ext) = path.extension() {
+        if ext == "md" || ext == "markdown" {
+            if let Some(stem) = path.file_stem() {
+                return stem.to_str();
+            }
+        }
+    }
+    None
 }
 
-fn is_markdown_entry(entry: &DirEntry) -> bool {
-    match entry.file_name().to_str() {
-        Some(s) => is_markdown_filename(s),
-        None => false,
-    }
-}
 // return true if the file is a source file for markdown to html conversion
 fn is_hidden(entry: &DirEntry) -> bool {
     entry
@@ -68,41 +72,57 @@ fn is_hidden(entry: &DirEntry) -> bool {
         .unwrap_or(false)
 }
 
-fn process_files(inpath: &str, outpath: &str) -> std::io::Result<()> {
+fn convert_one_file<W: Write>(source_path: &Path, out_writer: W) -> std::io::Result<()> {
+    Ok(())
+}
+
+fn process_files(inpath: &str, outpath_str: &str) -> std::io::Result<()> {
+    let outpath = Path::new(outpath_str);
     let walker = WalkDir::new(inpath).follow_links(true).into_iter();
-    for entry in walker.filter_entry(|e| !is_hidden(e)) {
-        let e = entry?;
-        if is_markdown_entry(&e) {
-            println!("{}", e.path().display());
+    for entry_result in walker.filter_entry(|e| !is_hidden(e)) {
+        let entry = entry_result?;
+        let path = entry.path();
+        if let Some(filename) = markdown_filename(&path) {
+            println!("{}", path.display());
+            let mut f = fs::File::open(path).expect("file not found");
+            let mut markdown_input = String::new();
+            f.read_to_string(&mut markdown_input)
+                .expect("error reading file");
+            info!("input: \n{}", markdown_input);
+
+            // Set up options and parser.
+            let mut options = Options::empty();
+            // Strikethroughs are not part of the CommonMark standard
+            // so must be enabled explicitly (TODO: maybe configure?)
+            options.insert(Options::ENABLE_STRIKETHROUGH);
+            let parser = MarkdownParser::new_ext(&markdown_input, options);
+
+            // let out_filename = format!("{}.html", filename);
+            let out_filepath = outpath.join(format!("{}.html", filename));
+            // TODO: match directory structure
+            // Write to a file
+            let out_file = fs::OpenOptions::new()
+                .write(true)
+                .create(true)
+                .open(out_filepath)
+                .expect("could not open output file");
+
+            let writer = BufWriter::new(out_file);
+            html::write_html(writer, parser).expect("unable to write to file");
+            info!("HTML file written!");
         }
     }
 
     // iterate over all files
-    let path = "markdown/index.md";
-    let mut f = fs::File::open(path).expect("file not found");
+    // let path = "markdown/index.md";
+    // let mut f = fs::File::open(path).expect("file not found");
 
-    let mut markdown_input = String::new();
-    f.read_to_string(&mut markdown_input)
-        .expect("error reading file");
+    // let mut markdown_input = String::new();
+    // f.read_to_string(&mut markdown_input)
+    //     .expect("error reading file");
 
-    info!("input: \n{}", markdown_input);
-    // Set up options and parser.
-    let mut options = Options::empty();
-    // Strikethroughs are not part of the CommonMark standard
-    // so must be enabled explicitly (TODO: maybe configure?)
-    options.insert(Options::ENABLE_STRIKETHROUGH);
-    let parser = MarkdownParser::new_ext(&markdown_input, options);
+    // info!("input: \n{}", markdown_input);
 
-    // Write to a file
-    let out_file = fs::OpenOptions::new()
-        .write(true)
-        .create(true)
-        .open(".site/test.html")
-        .expect("could not open output file");
-
-    let writer = BufWriter::new(out_file);
-    html::write_html(writer, parser).expect("unable to write to file");
-    info!("HTML file written!");
     Ok(())
 }
 
